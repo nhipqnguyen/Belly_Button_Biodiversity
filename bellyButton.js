6cb15de1bf511ff64e961858e909f547e592a356
Nhi Nguyen <NNguyen@ocjones.com>
2021-07-14 16:42:24 -0700

    completed 12.4.2

--- bellyButton.js
+++
@@ -1,3 +0,0 @@
-d3.json("samples.json").then(function(data){
-    console.log("hello");
-});
---
+++ practice/belly_button_practice/bellyButton_practice.js
@@ -0,0 +1,23 @@
+// retrieve data from samples.json
+d3.json("samples.json").then(function(data){
+    console.log(data);
+});
+
+// extract only the wfreq (weekly belly button washing frequency), 
+// of each person into a new array,
+d3.json("samples.json").then(function(data){
+    wfreq = data.metadata.map(person =>
+    // sort the wfreq array in descending order
+    person.wfreq).sort((a,b) => b - a);
+    // delete null values from the sorted wfreq array
+    filteredWfreq = wfreq.filter(element => element !=
+null);
+    console.log(filteredWfreq);
+});
+
+// display the metadata of the first individual in the dataset
+d3.json("samples.json").then(function(data){
+    firstPerson = data.metadata[0];
+    Object.entries(firstPerson).forEach(([key, value]) =>
+      {console.log(key + ': ' + value);});
+});
